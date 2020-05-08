c7828493ceab74da6b24eef08f9562965b012fa3
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-05-08 23:18:15 +0530

    created : unit tests for format function, fixed : package.json for testing, php.js for minor mistakes

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,17 +1,75 @@
 require('amd-loader');
-
 // for tests that need RequireJS
 define(function(require, exports, module) {
   'use strict';
   const phpjs = require('../thirdparty/php');
-
+  const format = require('../thirdparty/php').format;
   describe('thirdparty/php.js', () => {
     it('should expose a formatter method', () => {
       expect(phpjs.formatter).toBeDefined();
     });
 
-    describe('tests for format with options', () =>{
-      // tests
+    const customConfiguration = {
+      'Style': 'GNU',
+      'Indent': 'Space',
+      'Remove_all_comments': false,
+      'Remove_empty_lines': true,
+      'Make_long_opening_tag': true,
+      'Space_inside_brackets': true,
+      'Space_inside_blocks': true,
+      'Space_around_operators': true,
+    };
+    describe('Testing format functions using default configuration => ', () =>{
+      describe('Testing formatting for comments =>', () =>{
+        it('Should keep the single line-comment untouched', ()=>{
+          const code = '// a sample comment';
+          expect(format(code, customConfiguration)).toBe(code);
+        });
+        it('Should keep the # comment untouched', ()=>{
+          const code = '# a sample comment';
+          expect(format(code, customConfiguration)).toBe(code);
+        });
+        it('Should keep the multi-line comment untouched', ()=>{
+          const code = '/* a sample comment\n a multi line comment*/';
+          expect(format(code, customConfiguration)).toBe(code);
+        });
+      });
+      describe('Testing formatting for empty-lines =>', ()=>{
+        it('Should remove empty lines', ()=>{
+          const initCode = 'line_1\n\nline_2';
+          const finCode = 'line_1\nline_2';
+          expect(format(initCode, customConfiguration)).toBe(finCode);
+        });
+      });
+      describe('Testing formatting for short opening tags =>', ()=>{
+        it('Should replace short opening tags with long opening tags', ()=>{
+          const initCode = '<?\nSOME_CODE\n?>';
+          const finCode = '<?php\nSOME_CODE\n?>';
+          expect(format(initCode, customConfiguration)).toBe(finCode);
+        });
+      });
+      describe('Testing formatting for putting space inside brackets =>', ()=>{
+        it('Should put space inside () brackets', ()=>{
+          const initCode = 'some_code(some_more_code)';
+          const finCode = 'some_code( some_more_code )';
+          expect(format(initCode, customConfiguration)).toBe(finCode);
+        });
+        it('Should put space inside {} brackets', ()=>{
+          const initCode = 'some_code{some_more_code}';
+          const finCode = 'some_code{ some_more_code }';
+          expect(format(initCode, customConfiguration)).toBe(finCode);
+        });
+        it('Should put space inside [] brackets', ()=>{
+          const initCode = 'some_code[some_more_code]';
+          const finCode = 'some_code[ some_more_code ]';
+          expect(format(initCode, customConfiguration)).toBe(finCode);
+        });
+        it('Should put handle nested brackets ', ()=>{
+          const initCode = 'some_code[{some_more_code}(some_more_code)]';
+          const finCode = 'some_code[ { some_more_code }( some_more_code ) ]';
+          expect(format(initCode, customConfiguration)).toBe(finCode);
+        });
+      });
     });
   });
 });
